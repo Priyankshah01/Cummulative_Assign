112cc4e716243f5a93f2f71e669d699a15e23672
Priyank Shah <103551838+Priyankshah01@users.noreply.github.com>
2025-03-22 11:05:31 -0400

    Update TeacherPageController.cs

--- Cummulative_Assign/Controllers/TeacherPageController.cs
+++ Cummulative_Assign/Controllers/TeacherPageController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Cummulative_Assign.Models;
@@ -7,11 +7,20 @@ using MySql.Data.MySqlClient;
 
 namespace Cummulative_Assign.Controllers
 {
+    /// <summary>
+    /// Controller for handling web pages related to teachers.
+    /// This controller interacts with the TeacherAPIController to retrieve teacher data.
+    /// </summary>
     public class TeacherPageController : Controller
     {
         private readonly SchoolDbContext cummulative_assign1 = new SchoolDbContext();
 
-        // GET: TeacherPage/List
+        /// <summary>
+        /// Displays a list of teachers based on an optional search key.
+        /// Retrieves data from the TeacherAPIController and passes it to the List view.
+        /// </summary>
+        /// <param name="SearchKey">Optional search parameter to filter teachers.</param>
+        /// <returns>A view displaying the list of teachers.</returns>
         public ActionResult List(string SearchKey)
         {
             TeacherAPIController controller = new TeacherAPIController();
@@ -19,12 +28,16 @@ namespace Cummulative_Assign.Controllers
             return View(Teachers);
         }
 
-        // GET: TeacherPage/Show/{id}
+        /// <summary>
+        /// Displays detailed information about a specific teacher.
+        /// Retrieves teacher details from the TeacherAPIController.
+        /// </summary>
+        /// <param name="id">The unique identifier of the teacher.</param>
+        /// <returns>A view displaying the details of the selected teacher.</returns>
         public ActionResult Show(int id)
         {
             TeacherAPIController controller = new TeacherAPIController();
             Teacher NewTeacher = controller.FindTeacher(id);
-
             return View(NewTeacher);
         }
     }
